0a07a48f007aa8558d6dd66bb4522dc8682dab96
Chris Stavrakis <chris.stavrakis@go1.com>
2023-07-23 15:35:10 +0930

    made ballon rise and stop

--- src/components/Platform.tsx
+++ src/components/Platform.tsx
@@ -1,13 +1,48 @@
 
 // import React, { } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Ref, useRef, useState } from "react";
+import { BufferGeometry, Material, Mesh, NormalBufferAttributes } from "three";
 // import * as THREE from '@three'
 
 export default function BalloonPlatform({position, ...props}) {
   const { nodes, materials } = useGLTF("/models/balloon-platform.glb");
+  
+  const [ locked, setLocked ] = useState(false);
+
+  const meshRef = useRef<Mesh>(null!);
+
+  
+  console.log(meshRef?.current?.position.y);
+
+  function moveBalloonUp() {
+
+    if (!locked) {
+      meshRef.current.position.y += 0.01;
+    }
+  
+  }
+ 
+  useFrame(() => { 
+   
+    const handleSpacebarPress = (event: KeyboardEvent) => {
+      if (event.code === 'Space') {
+        setLocked(true);
+      }
+    };
+
+    // attach the event listener
+    window.addEventListener('keydown', handleSpacebarPress);
+
+    moveBalloonUp();
+  
+  });
+
   return (
     <group {...props} dispose={null} position={position}>
         <mesh
+        ref={meshRef}
         castShadow
         receiveShadow
         geometry={nodes.Balloon_ballon_0006.geometry}
